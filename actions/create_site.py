8ba1988b2d0868d615856d32fb9d785f56430018
Shaylin <chettyshaylin@gmail.com>
2017-05-03 14:26:09 +1000

    Added the API call

--- actions/create_site.py
+++ actions/create_site.py
@@ -1,5 +1,6 @@
 import logging
-
+from requests.auth import HTTPBasicAuth
+import requests
 
 def create_site(parameters):
     """
@@ -13,6 +14,13 @@ def create_site(parameters):
         city = parameters["City"]
         country = parameters["Country"]["alpha-2"]
         country_name = parameters["Country"]["name"]
+        
+        //API Call Here//
+        url = 'https://monash.riverbed.cc/api/scm.config/1.0/org/org-Monash-d388075e40cf1bfd/sites'
+        data = {"id": "string", "name": "HQ", "org": "string", "longname": "string", "uplinks": ["string"], "networks": ["string"], "street_address": "Examplecorp, Root Road 14, 12345 Adminten", "city": city, "country": "DE", "timezone": "Europe/Berlin", "size": 0, "uid": "string"}
+        res = requests.post(url, data = data, auth=HTTPBasicAuth('Shaylin', 'sche259'))
+        
+        //Ends Here//
 
         speech = "{} created in {}, {}".format(site_type.capitalize(), city, country_name)
 
