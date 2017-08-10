525340eaecdca671471ba90d1fb95768b3a5df93
apua1 <apua1@student.monash.edu>
2017-08-10 20:05:15 +1000

    Fix unit test

--- actions/create_uplink.py
+++ actions/create_uplink.py
@@ -20,17 +20,14 @@ def create_uplink(api_auth, parameters):
         wan_name = parameters["Wans"]
 
     except KeyError as e:
-
         error_string = "Error processing createUplink intent. {0}".format(e)
-
         logging.error(error_string)
-
         return error_string
 
     # Get all the sites and check whether there is a site match given city and site type
     data_sites = api_auth.list_sites().json()
     print(data_sites)
-    site = ""
+    site = " "
     for  item in data_sites["items"]:
         if (city+site_type in item["id"]):
             site = item["id"]
@@ -38,40 +35,27 @@ def create_uplink(api_auth, parameters):
 
     # Get all the wans and check whether there is a wan match target wan user want the uplink to be created on
     data_wans = api_auth.list_wans().json()
-    default_wan = ""
-    wan = ""
+    wan = " "
     for item in data_wans["items"]:
-        print(item['name'])
-        print(wan_name)
-        if ("Internet"  == item["name"]):
-            default_wan = item["id"]
         if (wan_name  == item["name"]):
             wan = item["id"]
             break
-    #if the user doesn't specify a wan, use default wan internet
-    if (wan_name == ""):
-        wan = default_wan
-        wan_name = "Internet"
 
-    if (wan != ""):
-        if (site != ""):
-            res = api_auth.create_uplink(site, uplink_name, wan)
+    #calle create uplink api
+    res = api_auth.create_uplink(site, uplink_name, wan)
 
-            if res.status_code == 200:
-                speech = "An uplink called {} had created in site {}_{} to {} wan".format(uplink_name, city, site_type, wan_name)
-            elif res.status_code == 400:
-                speech = "Invalid parameters: {}".format(res.json()["error"]["message"])
-            elif res.status_code == 500:
-                speech = "Error: Could not create uplink"
-            else:
-                speech = "Error: Could not connect to Steelconnect"
-        else:
-            speech = "Invalid site {}_{}".format(city, site_type)
+    if res.status_code == 200:
+        speech = "An uplink called {} had created in site {}_{} to {} wan".format(uplink_name, city, site_type, wan_name)
+    elif res.status_code == 400:
+        speech = "Invalid parameters: {}".format(res.json()["error"]["message"])
+    elif res.status_code == 500:
+        speech = "Error: Could not create uplink"
     else:
-        speech = "Invalid wan {}".format(wan_name)
+        speech = "Error: Could not connect to Steelconnect"
 
     logging.debug(speech)
     print(speech)
+    print(res.status_code)
     return speech
 
 
--- tests/test_create_uplink.py
+++ tests/test_create_uplink.py
@@ -16,27 +16,36 @@ class TestCreateUplinks(unittest.TestCase):
 
     def test_melbourne_shop_success(self):
         mock_api = MagicMock()
-        #mock_api.create_uplink = MagicMock(name="create_uplink")
+        mock_api.create_uplink = MagicMock(name="create_uplink")
         mock_api.create_uplink.return_value = MagicMock(spec=requests.Response, status_code=200)
         mock_api.create_uplink.return_value.json.return_value = melbourne_shop_success_speech_response
         result = app.create_uplink(mock_api, melbourne_shop_parameters)
         #self.assertTrue(mock_api.create_uplink.called)
         self.assertEqual(melbourne_shop_success_speech_response, result)
 
-    def test_melbourne_shop_400(self):
+    def test_tonny_branch_invalid_site_400(self):
         mock_api = MagicMock()
-        #mock_api.create_uplink = MagicMock(name="create_uplink")
+        mock_api.create_uplink = MagicMock(name="create_uplink")
         mock_api.create_uplink.return_value = MagicMock(spec=requests.Response, status_code=400)
-        mock_api.create_uplink.return_value.json.return_value = melbourne_shop_400_api_response
-        result = app.create_uplink(mock_api, melbourne_shop_parameters)
+        mock_api.create_uplink.return_value.json.return_value = tonny_branch_invalid_site_400_api_response
+        result = app.create_uplink(mock_api, tonny_branch_parameters)
         #self.assertTrue(mock_api.create_uplink.called)
-        self.assertEqual(melbourne_shop_400_speech_response, result)
+        self.assertEqual(tonny_branch_invalid_site_400_speech_response, result)
+
+    def test_hahawan_invalid_wan_400(self):
+        mock_api = MagicMock()
+        mock_api.create_uplink = MagicMock(name="create_uplink")
+        mock_api.create_uplink.return_value = MagicMock(spec=requests.Response, status_code=400)
+        mock_api.create_uplink.return_value.json.return_value = hahawan_invalid_wan_400_api_response
+        result = app.create_uplink(mock_api, hahawan_parameters)
+        # self.assertTrue(mock_api.create_uplink.called)
+        self.assertEqual(hahawan_invalid_wan_400_speech_response, result)
 
     def test_melbourne_shop_500(self):
         mock_api =  MagicMock()
-        #mock_api.create_uplink = MagicMock(name="create_uplink")
+        mock_api.create_uplink = MagicMock(name="create_uplink")
         mock_api.create_uplink.return_value = MagicMock(spec=requests.Response, status_code=500)
-        mock_api.create_uplink.return_value.json.return_value = melbourne_shop_500_speech_response
+        mock_api.create_uplink.return_value.json.return_value = melbourne_shop_500_api_response
         result = app.create_uplink(mock_api, melbourne_shop_parameters)
         #self.assertTrue(mock_api.create_uplink.called)
         self.assertEqual(melbourne_shop_500_speech_response, result)
@@ -49,15 +58,3 @@ class TestCreateUplinks(unittest.TestCase):
         result = app.create_uplink(mock_api, melbourne_shop_parameters)
         #self.assertTrue(mock_api.create_uplink.called)
         self.assertEqual(melbourne_shop_404_speech_response, result)
-
-    def test_invalid_site(self):
-        mock_api = MagicMock()
-        mock_api.create_uplink = MagicMock(name="create_uplink")
-        result = app.create_uplink(mock_api, tokyo_branch_parameters)
-        self.assertEqual(invalid_site_speech_response, result)
-
-    def test_invalid_wan(self):
-        mock_api = MagicMock()
-        mock_api.create_uplink = MagicMock(name="create_uplink")
-        result = app.create_uplink(mock_api, hahawan_parameters)
-        self.assertEqual(invalid_wan_speech_response, result)
